705288de540b8de92999f850653077f1b6a9f9e5
Samtfm <sfmemail@gmail.com>
2017-08-30 13:54:47 -0700

    style display

--- src/components/screen/screen.js
+++ src/components/screen/screen.js
@@ -4,7 +4,7 @@ import './screen.css';
 class Screen extends Component {
   render(){
     return (
-      <div>
+      <div className='display'>
         <span className='number'>
           {this.props.number}
 
