3762a136db25e8e50e27ec2df73c62bfdafb540c
Samtfm <sfmemail@gmail.com>
2017-08-30 10:15:37 -0700

    rearrange files, hello world

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import Screen from './screen/screen';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Screen/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/screen/screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './screen.css';
+
+class Screen extends Component {
+  render(){
+    return (
+      <div>
+        07734
+      </div>
+    );
+  }
+}
+
+export default Screen;
