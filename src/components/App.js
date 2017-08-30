8e3212aa89d8d1bfabb1d3fd901a72df813de5f7
Samtfm <sfmemail@gmail.com>
2017-08-30 14:27:09 -0700

    restructure to pass basic test

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,21 @@ import logo from '../logo.svg';
 import './App.css';
 import ScreenContainer from './screen/screen_container';
 import KeypadContainer from './keypad/keypad_container';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import RootReducer from '../reducers/root_reducer';
 
 class App extends Component {
   render() {
+    const store = createStore(RootReducer);
     return (
-      <div className="App">
-        <ScreenContainer/>
-        <KeypadContainer/>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <ScreenContainer/>
+          <KeypadContainer/>
+        </div>
+      </Provider>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import RootReducer from './reducers/root_reducer';
 
-//wrap app in provider
-const Root = ({store}) => (
-  <Provider store = {store}>
-    <App />
-  </Provider>
-);
 
-//create store
-const store = createStore(RootReducer);
-window.store = store; //temporary save to window TODO: Delete
-
-//render root in root element
-ReactDOM.render(<Root store={store} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
