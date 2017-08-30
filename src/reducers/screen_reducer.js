a84d7d7fd96841c60631c214f619178e31ffabd9
Samtfm <sfmemail@gmail.com>
2017-08-30 11:52:09 -0700

    remove trailing zeroes

--- src/reducers/screen_reducer.js
+++ src/reducers/screen_reducer.js
@@ -1,12 +1,15 @@
 import { SET_SCREEN, PUSH_DIGIT } from '../actions/screen_actions';
 
 const ScreenReducer = (state = "0", action) => {
-  Object.freeze(state);
   switch (action.type) {
     case SET_SCREEN:
       return `${action.value}`;
     case PUSH_DIGIT:
-      return state + `${action.value}`;
+      if (state === "0" && action.value !== '.'){
+        return `${action.value}`;
+      } else {
+        return state + `${action.value}`;
+      }
     default:
       return state;
   }
